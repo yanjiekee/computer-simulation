65d78e8b6d0a5bfed3783035a9dfaa24ec9bb28d
yj_kee <keeyanjie2013@gmail.com>
2020-04-23 01:45:30 +0800

    Debug function for computer simulation

---
+++ include/debug.h
@@ -0,0 +1,15 @@
+// Adapted from Steve Gunn's debug.h, which was licensed under the Creative Commons Attribution License
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#define DEBUG_ENABLE 1
+#define STREAM_FILE stderr
+
+#define DEBUG_MESSAGE(message, ...) do{debugPrintf("%s:%d:%s() ", __FILE__+4, __LINE__, __FUNCTION__); debugPrintf(message, ##__VA_ARGS__); debugString("\n");break;} while (true)
+
+void debugPrintf(char* message, ...);
+void debugString(char* message);
---
+++ src/debug.cpp
@@ -0,0 +1,43 @@
+#include "debug.h"
+
+#ifndef DEBUG_ENABLE
+
+void debugString(char* message) {
+    return;
+}
+
+void debugPrintf(char* message, ...) {
+    return;
+}
+
+#else
+
+void debugString(char* message) {
+	fprintf(STREAM_FILE, message);
+    return;
+}
+
+void debugPrintf(char* message, ...) {
+	bool message_has_args = false;
+	for (uint8_t i = 0; true; i++) {
+		if (message[i] == 0)
+			break;
+		if (message[i] == '%') {
+			message_has_args = true;
+			break;
+		}
+	}
+	if (message_has_args) {
+		va_list args;
+		va_start(args, message);
+		char buffer[256];
+		vsprintf(buffer, message, args);
+		va_end(args);
+		debugString(buffer);
+	} else {
+		debugString(message);
+	}
+    return;
+}
+
+#endif
