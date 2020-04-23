8f198fb15923c90a6a9d4f10f7695227e480ac07
yj_kee <keeyanjie2013@gmail.com>
2020-04-24 03:55:16 +0800

    Added Memory module, tested and working

---
+++ include/Memory.h
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <iostream>
+
+#include "debug.h"
+#include "InstructionDecoder.h"
+
+#define BYTE_LENGTH 8                   // Based on the type used in m_register array, e.g. uint8_t
+#define WORD_LENGTH (4 * BYTE_LENGTH)   // 1 word = 32 bits, size of bus
+#define NO_OF_WORDS (UINT32_MAX + 1)    // INT32_MAX + 1 = 2^32
+#define NO_OF_BYTES (NO_OF_WORDS * BYTE_LENGTH)
+
+// Memory space allocation in bits (indexing, start at 0)
+#define MEMORY_RESERVED_LOWERLIMIT  0x00000000
+#define MEMORY_RESERVED_UPPERLIMIT  0x003fffff
+#define MEMORY_TEXT_LOWERLIMIT      0x00400000
+#define MEMORY_TEXT_UPPERLIMIT      0x0fffffff
+#define MEMORY_STATIC_LOWERLIMIT    0x10000000
+#define MEMORY_STATIC_UPPERLIMIT    0x1000ffff
+#define MEMORY_HEAP_LOWERLIMIT      0x10010000
+#define MEMORY_STACK_UPPERLIMIT     0x7fffffff
+
+// Memory space allocation in bytes (indexing)
+#define MEMORY_RESERVED_LOWERLIMIT_BYTE 0x00000000
+#define MEMORY_RESERVED_UPPERLIMIT_BYTE 0x0007ffff
+#define MEMORY_TEXT_LOWERLIMIT_BYTE     0x00080000
+#define MEMORY_TEXT_UPPERLIMIT_BYTE     0x01ffffff
+#define MEMORY_STATIC_LOWERLIMIT_BYTE   0x02000000
+#define MEMORY_STATIC_UPPERLIMIT_BYTE   0x02001fff
+#define MEMORY_HEAP_LOWERLIMIT_BYTE     0x02002000
+#define MEMORY_STACK_UPPERLIMIT_BYTE    0x0fffffff
+
+union WordToBytes {
+    uint32_t word;
+    uint8_t byte[4];
+};
+
+class Memory {
+public:
+    Memory();
+    Memory(uint32_t* p_busA, uint32_t* p_busB, bool* p_g_control);
+    void run();
+
+private:
+    uint8_t m_reg[NO_OF_BYTES];    // Byte addressed
+    uint32_t* m_p_busA;
+    uint32_t* m_p_busB;
+    bool* m_p_g_control;
+
+    uint32_t read(uint32_t address, int dataLength);
+    void write(uint32_t data, uint32_t address, int dataLength);
+};
--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,1 +1,90 @@
 #include "Memory.h"
+
+Memory::Memory()
+    : m_p_busA(NULL), m_p_busB(NULL), m_p_g_control(NULL) {
+    std::cerr << "ERROR: Memory object floating, no connection made" << std::endl;
+}
+
+Memory::Memory(uint32_t* p_busA, uint32_t* p_busB, bool* p_g_control)
+    : m_p_busA(p_busA), m_p_busB(p_busB), m_p_g_control(p_g_control) {
+    // No need initialise Heap, Stack or Static
+    // Store: Reserved & Text section of memory
+}
+
+void Memory::run() {
+    // Allow lb,sb vs lw,sw
+    int dataLength;
+    if (m_p_g_control[BYTE_ACCESS]) {
+        dataLength = BYTE_LENGTH;
+    } else {
+        dataLength = WORD_LENGTH;
+    }
+
+    if (m_p_g_control[MEM_WRITE]) {
+        write(*m_p_busB, *m_p_busA, dataLength);
+    }
+    else if (m_p_g_control[MEM_READ]) {
+        *m_p_busA = read(*m_p_busA, dataLength);
+    } else {
+        std::cerr << "Memory is not used" << std::endl;
+    }
+    // Question: Why do we need MEM_READ when we have MEM_WRITE?
+}
+
+uint32_t Memory::read(uint32_t address, int dataLength) {
+    if (dataLength == BYTE_LENGTH) {
+        if (address > MEMORY_STACK_UPPERLIMIT_BYTE) {
+            std::cerr << "ERROR: Address not within the range of memory" << std::endl;
+            return 0;
+        }
+        DEBUG_MESSAGE("READ: Address: %x, Data: %x", address, m_reg[address]);
+        return m_reg[address];
+    }
+    else if (dataLength == WORD_LENGTH) {
+        if (address > MEMORY_STACK_UPPERLIMIT_BYTE - 3) {
+            std::cerr << "ERROR: Address not within the range of memory" << std::endl;
+            return 0;
+        }
+        uint32_t word = 0;
+        for (int i = 0; i < 4; i++) {
+            word = word << 8;
+            word += m_reg[address + i];
+            DEBUG_MESSAGE("READ: Address: %x, Data: %x", (address + i), m_reg[address + i]);
+            // DEBUG_MESSAGE("READ: Current word: %x", word);
+        }
+        return word;
+    } else {
+        std::cerr << "ERROR: Access memory only by BYTE or WORD\n" << std::endl;
+        return 0;
+    }
+}
+
+void Memory::write(uint32_t data, uint32_t address, int dataLength) {
+    if (dataLength == BYTE_LENGTH) {
+        if (address > MEMORY_STACK_UPPERLIMIT_BYTE) {
+            std::cerr << "ERROR: Address not within the range of memory" << std::endl;
+            return;
+        }
+        m_reg[address] = (uint8_t)data; // Does it works? Tested and working
+        DEBUG_MESSAGE("WRITE: Address: %x, Data: %x", address, m_reg[address]);
+    }
+    else if (dataLength == WORD_LENGTH) {
+        if (address > MEMORY_STACK_UPPERLIMIT_BYTE - 3) {
+            std::cerr << "ERROR: Address not within the range of memory" << std::endl;
+            return;
+        }
+        // Use union to cut down word into bytes, because they are in the same memory space
+        WordToBytes wordToBytes;
+        wordToBytes.word = data;
+        for (int i = 0; i < 4; i++) {
+            // ToDo: Test this function after decoder is working
+            // https://stackoverflow.com/questions/18276225/convert-int-to-short-in-c
+            m_reg[address + i] = wordToBytes.byte[3 - i];
+            // Index to [4 - i] to be consistent with loading operation
+            // The MSB is stored in the byte closest to pointer
+            DEBUG_MESSAGE("WRITE: Address: %x, Data: %x", (address + i), wordToBytes.byte[3 - i]);
+        }
+    } else {
+        std::cerr << "ERROR: Access memory only by BYTE or WORD\n" << std::endl;
+    }
+}
